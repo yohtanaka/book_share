f2bd169cca0bbd21c32c222fd5e9a33800a99882
vagrant <vagrant@localhost.localdomain>
2019-03-02 06:59:12 +0000

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BookShare</title>
+</head>
+<body>
+    <h1>本をシェアするサイト</h1>
+    <ul>
+        <li>トルストイ</li>
+        <li>ドストエフスキー</li>
+    </ul>
+</body>
+</html>
