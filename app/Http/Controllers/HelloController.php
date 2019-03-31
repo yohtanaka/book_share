23e5b63f05fef668c6daa4b06174f63b01ceb7a8
root <root@localhost.localdomain>
2019-03-31 15:25:17 +0000

    青本の内容をコミット

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    // アクションの追加
+public function index(){
+return <<<EOF
+
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,100 @@
 */
 
 Route::get('/', function () {
-    return view('index');
-});
+		return view('index');
+		});
 
 Route::resource('book', 'BookController');
+
+/* 青本の内容をまとめる BlueBook */
+
+// デフォルトのルーティング
+Route::get('/BlueBook', function(){
+		return view('welcome');
+		}
+	  );
+
+Route::get('BlueBook/hello', function(){
+		return '
+		<html>
+		<body>
+		<h1>Hello!</h1>
+		<p>This is Smaple page</p>
+		</body>
+		</html>';
+		}
+	  );
+
+// ヒアドキュメントを使用した文字列の出力(P34 2-4)
+// Route情報は後勝ち。
+
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1 {font-size:100pt; text-align:right; color:#eee; margin: -40px 0px -50px 0px;}
+</style>
+</head>
+<body>
+<h1> Hello </h1>
+<p> This is sample page</p>
+<p> これはサンプルで作ったページです。 </p>
+</body>
+</html>
+EOF;
+
+Route::get('BlueBook/hello2_4',function()
+		use($html){
+		return $html;
+		}
+	  );
+
+// パラメータを利用した表示(P36 2-5)
+// 第一引数に、パラメータを組み込んだURLを用意してあげる。
+
+Route::get('BlueBook/hello2_5/{msg}',function($msg){
+		$html = <<<EOF
+		<html>
+		<head>
+		<title>Hello</title>
+		<style>
+		body {font-size:16pt; color:#999;}
+		h1 {font-size:100pt; text-align:right; color:#eee; margin: -40px 0px -50px 0px;}
+		</style>
+		</head>
+		<body>
+		<h1> Hello </h1>
+		<p>{$msg}</p>
+		<p> これはサンプルで作ったページです。 </p>
+		</body>
+		</html>
+		EOF;
+
+		return $html;
+		}
+	  );
+
+// 任意パラメータを利用したサンプル
+
+Route::get('BlueBook/hello2_5_2/{msg?}',function($msg = 'no message.'){
+		$html = <<<EOF
+		<html>
+		<head>
+		<title>Hello</title>
+		<style>
+		body {font-size:16pt; color:#999;}
+		h1 {font-size:100pt; text-align:right; color:#eee; margin: -40px 0px -50px 0px;}
+		</style>
+		</head>
+		<body>
+		<h1> Hello </h1>
+		<p>{$msg}</p>
+		<p> これはサンプルで作ったページです。 </p>
+		</body>
+		</html>
+		EOF;
+		return $html;
+		}
+	  );
